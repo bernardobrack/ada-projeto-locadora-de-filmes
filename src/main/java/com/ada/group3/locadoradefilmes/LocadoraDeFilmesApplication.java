f3479df884d37e67727e4029b01dbef98f88112e
Kauan Jasiskis <jasiskiskauan@gmail.com>
2024-04-23 15:44:24 -0300

    feat: ModelMapper created

--- src/main/java/com/ada/group3/locadoradefilmes/LocadoraDeFilmesApplication.java
+++ src/main/java/com/ada/group3/locadoradefilmes/LocadoraDeFilmesApplication.java
@@ -1,13 +1,19 @@
 package com.ada.group3.locadoradefilmes;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LocadoraDeFilmesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocadoraDeFilmesApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
