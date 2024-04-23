5e04178551078d2ba4e68f6fa8621194a175a0de
Kauan Jasiskis <jasiskiskauan@gmail.com>
2024-04-23 13:25:52 -0300

    Criacao classe filme conceito

---
+++ src/main/java/com/ada/group3/locadoradefilmes/modelo/filme/FilmeConceito.java
@@ -0,0 +1,26 @@
+package com.ada.group3.locadoradefilmes.modelo.filme;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class FilmeConceito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(columnDefinition = "UUID default RANDOM_UUID()")
+    private UUID uuid;
+    private String nome;
+    private String genero;
+    private String descricao;
+
+
+
+
+
+}
