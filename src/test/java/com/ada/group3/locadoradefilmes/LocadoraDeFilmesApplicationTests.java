5abaff2ebdcacd5801769bddc908413b59eac54f
Bernardo Brack <be.gbrack@gmail.com>
2024-04-22 21:09:49 -0300

    initialize project

---
+++ src/test/java/com/ada/group3/locadoradefilmes/LocadoraDeFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.group3.locadoradefilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraDeFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
