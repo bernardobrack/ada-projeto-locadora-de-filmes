3dc92dc464d324a3dd92b430798474da5e8313e0
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-04-24 17:48:14 -0300

    Feat: FilmeConteitoDTO created

---
+++ src/main/java/com/ada/group3/locadoradefilmes/modelo/filme/FilmeConceitoDto.java
@@ -0,0 +1,17 @@
+package com.ada.group3.locadoradefilmes.modelo.filme;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+public class FilmeConceitoDto {
+
+    private Long id;
+    private UUID uuid;
+    private String nome;
+    private String genero;
+    private String descricao;
+    //private List<FilmeRealDto> filmesReais;
+
+}
