e4040cc9df88e7380c00bd1ef0361ec1fda69e08
KauanJasiskis <142058588+KauanJasiskis@users.noreply.github.com>
2024-04-29 20:34:30 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO usuario (name, last_name, cpf, email, username, password, role)
-VALUES ('kauan', 'jasiskis', '12345678900', 'jasiskiskauan@gmail.com', 'kauan.123', '$2a$10$Vo18Be.iplWExbbx9CUtt.u.WJHeNQQWCYFjSmQK2uK4Bo3eZ.nJC', 'ADMIN');
+VALUES ('kauan', 'jasiskis', '12345678900', 'seuemail@gmail.com', 'kauan.123', '$2a$10$Vo18Be.iplWExbbx9CUtt.u.WJHeNQQWCYFjSmQK2uK4Bo3eZ.nJC', 'ADMIN');
