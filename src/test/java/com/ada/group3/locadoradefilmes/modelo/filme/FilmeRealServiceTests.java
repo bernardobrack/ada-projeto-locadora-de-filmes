1d5053720e64b8cfcbd9bf78b581440b0584f079
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-05-19 20:22:18 -0300

    feat: FilmeRealDTO tests. Intellij says its convered, JaCoCo says otherwise tho...

---
+++ src/test/java/com/ada/group3/locadoradefilmes/modelo/filme/FilmeRealServiceTests.java
@@ -0,0 +1,19 @@
+package com.ada.group3.locadoradefilmes.modelo.filme;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.UUID;
+
+public class FilmeRealServiceTests {
+
+    @Mock
+    FilmeRealRepository filmeRealRepository;
+    //Continuar isso depois!
+//    @Test
+//    public void listarFilmesRetornaOFilmeCorreto() {
+//        UUID uuid = UUID.randomUUID();
+//        filmeRealRepository.findByFilmeConceitoUuid(uuid);
+//    }
+//
+}
