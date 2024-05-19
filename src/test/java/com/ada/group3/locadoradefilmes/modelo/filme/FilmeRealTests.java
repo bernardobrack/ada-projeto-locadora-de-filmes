3a8126796b73ef3c69e8c157480cf1a2f3e606e1
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-05-19 19:30:28 -0300

    feat: 100% test converage on FilmeRealTests

--- src/test/java/com/ada/group3/locadoradefilmes/modelo/filme/FilmeRealTests.java
+++ src/test/java/com/ada/group3/locadoradefilmes/modelo/filme/FilmeRealTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 public class FilmeRealTests {
 
@@ -38,6 +39,92 @@ public class FilmeRealTests {
         FilmeConceito conceito = new FilmeConceito();
         filme.setFilmeConceito(conceito);
         assertSame(conceito, filme.getFilmeConceito());
+    }
+
+    @Test
+    public void deveriaRetornarIdCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        filmeReal.setId(1L);
+
+        assertEquals(1L, filmeReal.getId());
+    }
+
+    @Test
+    public void deveriaRetornarIsAlugadoCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        filmeReal.setAlugado(true);
+
+        assertTrue(filmeReal.isAlugado());
+    }
+
+    @Test
+    public void deveriaRetornarUuidCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        UUID uuid = UUID.randomUUID();
+        filmeReal.setUuid(uuid);
+
+        assertEquals(uuid, filmeReal.getUuid());
+    }
+
+    @Test
+    public void deveriaRetornarFilmeConceitoCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        FilmeConceito filmeConceito = new FilmeConceito();
+        filmeReal.setFilmeConceito(filmeConceito);
+
+        assertEquals(filmeConceito, filmeReal.getFilmeConceito());
+    }
+
+    @Test
+    public void deveriaRetornarAlugueisCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        List<Aluguel> alugueis = mock(List.class);
+        filmeReal.setAlugueis(alugueis);
+
+        assertEquals(alugueis, filmeReal.getAlugueis());
+    }
+
+    @Test
+    public void deveriaSetarIdCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        filmeReal.setId(1L);
+
+        assertEquals(1L, filmeReal.getId());
+    }
+
+    @Test
+    public void deveriaSetarIsAlugadoCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        filmeReal.setAlugado(true);
+
+        assertTrue(filmeReal.isAlugado());
+    }
+
+    @Test
+    public void deveriaSetarUuidCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        UUID uuid = UUID.randomUUID();
+        filmeReal.setUuid(uuid);
+
+        assertEquals(uuid, filmeReal.getUuid());
+    }
+
+    @Test
+    public void deveriaSetarFilmeConceitoCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        FilmeConceito filmeConceito = new FilmeConceito();
+        filmeReal.setFilmeConceito(filmeConceito);
+
+        assertEquals(filmeConceito, filmeReal.getFilmeConceito());
+    }
+
+    @Test
+    public void deveriaSetarAlugueisCorretamente() {
+        FilmeReal filmeReal = new FilmeReal();
+        List<Aluguel> alugueis = mock(List.class);
+        filmeReal.setAlugueis(alugueis);
+
+        assertEquals(alugueis, filmeReal.getAlugueis());
     }
 
 }
